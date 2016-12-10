c6178d5e1a35cb07a440463cae20e70a5c76f50a
shilpareddy123 <shilpareddy.2793@gmail.com>
2016-12-10 18:25:07 +0530

    single page angular

---
+++ config/config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyD1ULJW3RUA4CZYsdcpD6qvwn-JOaJ91ow",
+  authDomain: "fir-f7dce.firebaseapp.com",
+  databaseURL: "https://fir-f7dce.firebaseio.com",
+  storageBucket: "fir-f7dce.appspot.com",
+  messagingSenderId: "271499280575"
+};
+firebase.initializeApp(config);
